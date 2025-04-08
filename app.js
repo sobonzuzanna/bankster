92b2ebb4c717775f321fdc8b481fee3b5911bae3
Zuzanna Soboń <133899490+sobonzuzanna@users.noreply.github.com>
2025-04-08 19:45:35 +0200

    finished: rotating logo section

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
-const menu = document.querySelector("#mobile-menu")
-const menuLinks = document.querySelector(".navbar__menu")
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".navbar__menu");
 
 menu.addEventListener("click", function () {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle("active");
 
     // console.log("it worked");
-})
+});
+
+// foo to make logos move
+const logos = document.querySelector(".rotating-logos__track").cloneNode(true);
+document.querySelector(".rotating-logos").appendChild(logos);
+
